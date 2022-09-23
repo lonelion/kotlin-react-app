61b8777c294412fadde56d327bb5bf51e24f499d
root <root@platform-api-service-647b6755b4-pwngh>
2022-09-23 14:48:42 +0000

    Initial commit

---
+++ src/main/kotlin/kotlinreact/KotlinReactApp.kt
@@ -0,0 +1,17 @@
+package kotlinreact
+
+import com.equo.application.api.IEquoApplication
+import com.equo.application.model.EquoApplicationBuilder
+import com.equo.dev.EquoApp
+import org.osgi.service.component.annotations.Component
+
+fun main(args: Array<String>) {
+    EquoApp.launch()
+}
+
+@Component
+class KotlinReactApp : IEquoApplication {
+    override fun buildApp(appBuilder: EquoApplicationBuilder?): EquoApplicationBuilder {
+        return appBuilder!!.withUI("index.html").start();
+    }
+}
